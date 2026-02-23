c15a202b2d989e94308fca68b2421b1ebb5193b6
undone0603 <undone.k@gmail.com>
2026-02-23 23:08:52 +0000

    zk

--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,6 @@
 export { Header } from './Header';
 export { Footer } from './Footer';
-export { ModeSelector } from './ModeSelector';
+export { ModeSelector } from '../app/ModeSelector';
 export { QRGenerator } from './QRGenerator';
 export { QRDisplay } from './QRDisplay';
 export { Providers } from './Providers';
