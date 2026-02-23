d727203e680244a7e5a86983a4806eb29636aade
undone0603 <undone.k@gmail.com>
2026-02-23 01:28:35 -0800

    Create next.config.js with initial configuration

---
+++ next.config.js
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  typescript: {
+    tsconfigPath: './tsconfig.json',
+  },
+  eslint: {
+    dirs: ['app', 'components', 'lib'],
+  },
+};
+
+module.exports = nextConfig;
